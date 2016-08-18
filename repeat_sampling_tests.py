096deb622c1e6e973c310f6628ad0b7526eb68b2
Alden <adderan@ucsc.edu>
2016-08-18 14:37:42 -0700

    toil script working

--- repeat_sampling_tests.py
+++ repeat_sampling_tests.py
@@ -1,6 +1,7 @@
 import argparse
 import re
 import os
+import shutil
 
 from toil.common import Toil
 from toil.job import Job
@@ -16,14 +17,14 @@ def makeRawSeedCounts(job, options, sequenceID):
     job.fileStore.logToMaster("Getting seed counts from lastz...")
     sequence = job.fileStore.readGlobalFile(sequenceID)
     rawSeedCounts = job.fileStore.getLocalTempFile()
-    os.system("cactus_lastz --tableonly=count %s > %s" % (sequence, rawSeedCounts))
+    os.system("cPecanLastz --tableonly=count %s > %s" % (sequence, rawSeedCounts))
     return job.fileStore.writeGlobalFile(rawSeedCounts)
 
 def makeSeedScoresTable(job, options, rawSeedCountsIDs):
     job.fileStore.logToMaster("Making seed scores table...")
     rawSeedCountsFiles = [job.fileStore.readGlobalFile(rawSeedCountsID) for rawSeedCountsID in rawSeedCountsIDs]
     seedScoresTable = job.fileStore.getLocalTempFile()
-    os.system("cactus_blast_makeSeedScoresTable --countThreshold=%i --seedScoresFile=%s %s" 
+    os.system("cactus_blast_makeSeedScoreTable --countThreshold=%i --seedScoresFile=%s %s" 
             % (options.countThreshold, seedScoresTable, " ".join(rawSeedCountsFiles)))
     return job.fileStore.writeGlobalFile(seedScoresTable)
 
@@ -44,15 +45,29 @@ def parseStats(job, options, statsID):
     """Parse the lastz stats file.
     """
     statsFile = job.fileStore.readGlobalFile(statsID)
-    stat = re.complie("(\S+): (\d+)")
 
     stats = dict()
-    for line in open(stats):
+    for line in open(statsFile, 'r'):
         line = line.strip()
-        m = stat.match(line)
-        if m:
-            stats[m.group(0)] = m.group(1)
+        colon = line.find(":")
+        if not colon:
+            continue
+        statName = line[:colon]
+        statValue = line[colon+1:]
+        stats[statName] = statValue
+
+    job.fileStore.logToMaster("Stats dict:")
+    job.fileStore.logToMaster(" ".join(stats.keys()))
     return stats
+
+def runLastz(job, options, seqID1, seqID2):
+    job.fileStore.logToMaster("Running LASTZ without seed sampling...")
+    seq1 = job.fileStore.readGlobalFile(seqID1)
+    seq2 = job.fileStore.readGlobalFile(seqID2)
+    alignmentsFile = job.fileStore.getLocalTempFile()
+    statsFile = job.fileStore.getLocalTempFile()
+    os.system("cPecanLastz %s[multiple] %s[multiple] --stats=%s > %s" % (seq1, seq2, statsFile, alignmentsFile))
+    return job.fileStore.writeGlobalFile(statsFile)
 
 
 def runSampledLastz(job, options, seqID1, seqID2, seedScoresID):
@@ -62,16 +77,20 @@ def runSampledLastz(job, options, seqID1, seqID2, seedScoresID):
     seedScores = job.fileStore.readGlobalFile(seedScoresID)
     alignmentsFile = job.fileStore.getLocalTempFile()
     statsFile = job.fileStore.getLocalTempFile()
-    os.system("cactus_lastz %s[unmask][multiple] %s[unmask][multiple] --seedScoresFile=%s --stats=%s --sampleSeedThreshold=%i > %s" % (seq1, seq2, seedScores, options.sampleSeedThreshold, statsFile, alignmentsFile))
+    os.system("cPecanLastz %s[unmask] %s[unmask] --seedCountsTable=%s --sampleSeedThreshold=%i --stats=%s > %s" % (seq1, seq2, seedScores, options.sampleSeedThreshold, statsFile, alignmentsFile))
     return job.fileStore.writeGlobalFile(statsFile)
 
 
-def printThresholdVsHSPs(options, thresholdValues, statsDicts):
+def printThresholdVsHSPs(job, options, thresholdValues, statsDicts):
+    job.fileStore.logToMaster("Writing output file...")
     outputFile = job.fileStore.getLocalTempFile()
     with open(outputFile, 'w') as fh:
         for threshold, stats in zip(thresholdValues, statsDicts):
-            fh.write("%i %i\n" % (threshold, stats["HSPs"]))
-    return job.fileStore.writeGlobalFile(outputFile)
+            fh.write("%i %i\n" % (threshold, int(stats["HSPs"])))
+    #os.system("cp %s %s" % (outputFile, os.path.abspath(options.outputFile)))
+    outputID = job.fileStore.writeGlobalFile(outputFile)
+    job.fileStore.exportFile(outputID, toURL(options.outputFile))
+    #shutil.copyfile(outputFile, options.outputFile)
     
     
 def plotThreshold(options, sequence):
@@ -80,10 +99,22 @@ def plotThreshold(options, sequence):
         seedCountsJob = Job.wrapJobFn(makeRawSeedCounts, options, sequenceID)
         seedScoreTableJob = Job.wrapJobFn(makeSeedScoresTable, options, rawSeedCountsIDs = [seedCountsJob.rv()])
         chunksJob = Job.wrapJobFn(getChunks, options, [sequenceID])
-        lastzJob = Job.wrapJobFn(runSampledLastz, chunksJob.rv(0), chunksJob.rv(0), seedScoreTableJob.rv())
+        lastzJob = Job.wrapJobFn(runSampledLastz, options, chunksJob.rv(0), chunksJob.rv(0), seedScoreTableJob.rv())
         parseStatsJob = Job.wrapJobFn(parseStats, options, statsID=lastzJob.rv())
         printOutputJob = Job.wrapJobFn(printThresholdVsHSPs, options, [2], [parseStatsJob.rv()])
 
+
+        #           1 --> 3 ----
+        #           |           |---> 4 ---> 5
+        #           2 ----------
+        
+
+        #seedCountsJob.addFollowOn(seedScoreTableJob)
+        #seedCountsJob.addChild(chunksJob)
+        #chunksJob.addFollowOn(lastzJob)
+        #seedScoreTableJob.addFollowOn(lastzJob)
+        #lastzJob.addFollowOn(parseStatsJob)
+        #parseStatsJob.addFollowOn(printOutputJob)
         seedCountsJob.addFollowOn(seedScoreTableJob)
         seedScoreTableJob.addFollowOn(chunksJob)
         chunksJob.addFollowOn(lastzJob)
@@ -91,9 +122,6 @@ def plotThreshold(options, sequence):
         parseStatsJob.addFollowOn(printOutputJob)
 
         toil.start(seedCountsJob)
-
-        toil.exportFile(printOutputJob.rv(), toURL(options.outputFile))
-
 
 def main():
     parser = argparse.ArgumentParser()
